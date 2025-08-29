a41f7fd7496c95a20467893357216183d5006858
ArmandoEnrique1010K <enrique1010k@gmail.com>
2025-08-28 22:55:54 -0500

    02 Random Color

--- 02-random-color/src/App.jsx
+++ 02-random-color/src/App.jsx
@@ -1,9 +1,8 @@
-import RandomColor from "./components/RandomColor"
+import RandomColor from "./components/RandomColor";
 function App() {
-
-  return (
-    <RandomColor />
-  )
+  // Renderiza el componente Accordian
+  // Renderizar significa ejecutar la función principal del componente una vez y mostrar el contenido HTML
+  return <RandomColor />;
 }
 
-export default App
+export default App;
